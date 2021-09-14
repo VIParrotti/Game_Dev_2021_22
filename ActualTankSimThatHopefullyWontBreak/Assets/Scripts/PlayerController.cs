e82aa380d419809e7703eaeae4a535eee1586707
VIParrotti <victoria.p805@stu.nebo.edu>
2021-09-14 14:13:47 -0600

    Finished the game!!!!

---
+++ ActualTankSimThatHopefullyWontBreak/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 25.0f;
+    private float turnSpeed = 50.0f;
+    private float hInput;
+    private float vInput;
+
+    //Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput);
+        transform.Rotate(Vector3.up,turnSpeed * hInput * Time.deltaTime);
+
+    }
+}
