f8d5b11d2982bd658cfb5f0a178e1aa559029b7b
VIParrotti <victoria.p805@stu.nebo.edu>
2021-09-21 14:19:17 -0600

    More code

--- Prototype2 - theDownUnder/Assets/Scripts/LinkControls.cs
+++ Prototype2 - theDownUnder/Assets/Scripts/LinkControls.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class LinkControls : MonoBehaviour
 {
     //Variable
-    public float speed = 2.0f;
+    public float speed = 5.0f;
     public float turnSpeed = 10.0f;
     public float hInput;
     public float vInput;
+
+    public float xRange = 8.91f;
+    public float yRange = 4.46f;
 
     // Update is called once per frame
     void Update()
@@ -17,7 +20,28 @@ public class LinkControls : MonoBehaviour
         vInput = Input.GetAxis("Vertical");
 
         // Link would like to move in different directions
-        transform.Translate(Vector3.forward * speed * Time.deltaTime * hInput);
-        transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput);
+        transform.Translate(Vector3.right * speed * Time.deltaTime * hInput);
+        transform.Translate(Vector3.up * speed * Time.deltaTime * vInput);
+
+        //left and right boundaries
+        if(transform.position.x > xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        
+        //top and bottom boundaries
+        if(transform.position.y > yRange)
+        {
+            transform.position = new Vector3(transform.position.x, yRange, transform.position.z);
+        }
+        if(transform.position.y < -yRange)
+        {
+            transform.position = new Vector3(transform.position.x, -yRange, transform.position.z);
+        }
+    
     }
 }
