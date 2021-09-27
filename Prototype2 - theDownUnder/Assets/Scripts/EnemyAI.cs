f00430e9c3b11562f687a0ce1725f8e2463dd96b
VIParrotti <victoria.p805@stu.nebo.edu>
2021-09-27 13:37:05 -0600

    Enemy moves

--- Prototype2 - theDownUnder/Assets/Scripts/EnemyAI.cs
+++ Prototype2 - theDownUnder/Assets/Scripts/EnemyAI.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public Transform player;
+    public float moveSpeed = 5.0f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+    
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -25,3 +44,4 @@ public class EnemyAI : MonoBehaviour
         
     }
 }
+   
